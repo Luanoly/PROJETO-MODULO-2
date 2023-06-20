40460a8da3f311dd38db456007608c8e297c7048
Luanoly <los8@aluno.ifal.edu.br>
2023-06-20 13:50:13 -0300

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const tableBodyElemnt = document.querySelector('#userTable');
 fetch('http://localhost:5000/aluno')
     .then(response => response.json())
     .then(data => {
-        const users = data.results;
+        const users = data;
         users.forEach(user => {
             const row = document.createElement('tr');
             const nome = document.createElement('td');
@@ -22,5 +22,12 @@ fetch('http://localhost:5000/aluno')
             row.appendChild(cpf);
 
             tableBodyElemnt.appendChild(row);
+
+            async function mostrar() {
+                const response = await
+                    fetch('https://randomuser.me/api');
+                const jsonData = await response.json();
+                console.log(jsonData);
+            }
         });
     })
