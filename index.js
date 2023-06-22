bb7b225db090cf53d8a0eeb67e9b2382442083b2
Luanoly <los8@aluno.ifal.edu.br>
2023-06-22 14:56:38 -0300

    al

--- index.js
+++ index.js
@@ -2,40 +2,40 @@ const tableBodyElement = document.querySelector('#userTable');
 
 let botao = document.getElementById('botao1');
 
-botao.addEventListener('click', function(){
+botao.addEventListener('click', function () {
 
     fetch('https://randomuser.me/api/?nat=BR&results=15&seed=SL&page=1')
 
-    .then(response => response.json())
+        .then(response => response.json())
 
-    .then(data => {
-        const users = data.results;
-        users.forEach(user => {
-            const row = document.createElement('tr');
-            const nome = document.createElement('td');
-            const cidade = document.createElement('td');
-            const email = document.createElement('td');
-            const celular = document.createElement('td');
-            const foto = document.createElement('img');
+        .then(data => {
+            const users = data.results;
+            users.forEach(user => {
+                const row = document.createElement('tr');
+                const nome = document.createElement('td');
+                const cidade = document.createElement('td');
+                const email = document.createElement('td');
+                const celular = document.createElement('td');
+                const foto = document.createElement('img');
 
-            nome.textContent = user.name.first;
-            cidade.textContent = user.location.city;
-            email.textContent = user.email;
-            celular.textContent = user.phone;
-            foto.src = user.picture.large;
+                nome.textContent = user.name.first;
+                cidade.textContent = user.location.city;
+                email.textContent = user.email;
+                celular.textContent = user.phone;
+                foto.src = user.picture.large;
 
-            row.appendChild(foto);
-            row.appendChild(nome);
-            row.appendChild(cidade);
-            row.appendChild(email);
-            row.appendChild(celular);
-            tableBodyElement.appendChild(row);
+                row.appendChild(foto);
+                row.appendChild(nome);
+                row.appendChild(cidade);
+                row.appendChild(email);
+                row.appendChild(celular);
+                tableBodyElement.appendChild(row);
+            });
+        })
+
+        .catch(error => {
+            console.log('Ocorreu um erro:', error);
         });
-    })
-
-    .catch(error => {
-        console.log('Ocorreu um erro:', error);
-    });
 
 });
 
@@ -43,39 +43,49 @@ botao.addEventListener('click', function(){
 
 let botao2 = document.getElementById('botao2');
 
-botao2.addEventListener('click', function(){
+botao2.addEventListener('click', function () {
 
     fetch('https://randomuser.me/api/?nat=BR&results=15&seed=SL&page=22')
-  
-    .then(response => response.json())
 
-    .then(data => {
-        const users = data.results;
-        users.forEach(user => {
-            const row = document.createElement('tr');
-            const nome = document.createElement('td');
-            const cidade = document.createElement('td');
-            const email = document.createElement('td');
-            const celular = document.createElement('td');
-            const foto = document.createElement('img');
+        .then(response => response.json())
 
-            nome.textContent = user.name.first;
-            cidade.textContent = user.location.city;
-            email.textContent = user.email;
-            celular.textContent = user.phone;
-            foto.src = user.picture.large;
+        .then(data => {
+            const users = data.results;
+            tableBodyElement.innerHTML =
+             `  <thead>
+                <tr>
+                    <th>imagem</th>
+                    <th>Nome</th>
+                    <th>Cidade</th>
+                    <th>E-mail</th>
+                    <th>Celular</th>
+                </tr>
+                </thead>`;
+            users.forEach(user => {
+                const row = document.createElement('tr');
+                const nome = document.createElement('td');
+                const cidade = document.createElement('td');
+                const email = document.createElement('td');
+                const celular = document.createElement('td');
+                const foto = document.createElement('img');
 
-            row.appendChild(foto);
-            row.appendChild(nome);
-            row.appendChild(cidade);
-            row.appendChild(email);
-            row.appendChild(celular);
-            tableBodyElement.appendChild(row);
+                nome.textContent = user.name.first;
+                cidade.textContent = user.location.city;
+                email.textContent = user.email;
+                celular.textContent = user.phone;
+                foto.src = user.picture.large;
+
+                row.appendChild(foto);
+                row.appendChild(nome);
+                row.appendChild(cidade);
+                row.appendChild(email);
+                row.appendChild(celular);
+                tableBodyElement.appendChild(row);
+            });
+        })
+        .catch(error => {
+            console.log('Ocorreu um erro:', error);
         });
-    })
-    .catch(error => {
-        console.log('Ocorreu um erro:', error);
-    });
 
 });
 
@@ -83,39 +93,49 @@ botao2.addEventListener('click', function(){
 
 let botao3 = document.getElementById('botao3');
 
-botao3.addEventListener('click', function(){
+botao3.addEventListener('click', function () {
 
     fetch('https://randomuser.me/api/?nat=BR&results=15&seed=SL&page=33')
 
-    .then(response => response.json())
+        .then(response => response.json())
 
-    .then(data => {
-        const users = data.results;
-        users.forEach(user => {
-            const row = document.createElement('tr');
-            const nome = document.createElement('td');
-            const cidade = document.createElement('td');
-            const email = document.createElement('td');
-            const celular = document.createElement('td');
-            const foto = document.createElement('img');
+        .then(data => {
+            const users = data.results;
+            tableBodyElement.innerHTML =
+             `  <thead>
+                <tr>
+                    <th>imagem</th>
+                    <th>Nome</th>
+                    <th>Cidade</th>
+                    <th>E-mail</th>
+                    <th>Celular</th>
+                </tr>
+                </thead>`;
+            users.forEach(user => {
+                const row = document.createElement('tr');
+                const nome = document.createElement('td');
+                const cidade = document.createElement('td');
+                const email = document.createElement('td');
+                const celular = document.createElement('td');
+                const foto = document.createElement('img');
 
-            nome.textContent = user.name.first;
-            cidade.textContent = user.location.city;
-            email.textContent = user.email;
-            celular.textContent = user.phone;
-            foto.src = user.picture.large;
+                nome.textContent = user.name.first;
+                cidade.textContent = user.location.city;
+                email.textContent = user.email;
+                celular.textContent = user.phone;
+                foto.src = user.picture.large;
 
-            row.appendChild(foto);
-            row.appendChild(nome);
-            row.appendChild(cidade);
-            row.appendChild(email);
-            row.appendChild(celular);
-            tableBodyElement.appendChild(row);
+                row.appendChild(foto);
+                row.appendChild(nome);
+                row.appendChild(cidade);
+                row.appendChild(email);
+                row.appendChild(celular);
+                tableBodyElement.appendChild(row);
+            });
+        })
+        .catch(error => {
+            console.log('Ocorreu um erro:', error);
         });
-    })
-    .catch(error => {
-        console.log('Ocorreu um erro:', error);
-    });
 
 });
 
@@ -123,39 +143,39 @@ botao3.addEventListener('click', function(){
 
 let botao4 = document.getElementById('botao4');
 
-botao4.addEventListener('click', function(){
+botao4.addEventListener('click', function () {
 
     fetch('https://randomuser.me/api/?nat=BR&results=15&seed=SL&page=4')
 
-    .then(response => response.json())
+        .then(response => response.json())
 
-    .then(data => {
-        const users = data.results;
-        users.forEach(user => {
-            const row = document.createElement('tr');
-            const nome = document.createElement('td');
-            const cidade = document.createElement('td');
-            const email = document.createElement('td');
-            const celular = document.createElement('td');
-            const foto = document.createElement('img');
+        .then(data => {
+            const users = data.results;
+            users.forEach(user => {
+                const row = document.createElement('tr');
+                const nome = document.createElement('td');
+                const cidade = document.createElement('td');
+                const email = document.createElement('td');
+                const celular = document.createElement('td');
+                const foto = document.createElement('img');
 
-            nome.textContent = user.name.first;
-            cidade.textContent = user.location.city;
-            email.textContent = user.email;
-            celular.textContent = user.phone;
-            foto.src = user.picture.large;
+                nome.textContent = user.name.first;
+                cidade.textContent = user.location.city;
+                email.textContent = user.email;
+                celular.textContent = user.phone;
+                foto.src = user.picture.large;
 
-            row.appendChild(foto);
-            row.appendChild(nome);
-            row.appendChild(cidade);
-            row.appendChild(email);
-            row.appendChild(celular);
-            tableBodyElement.appendChild(row);
+                row.appendChild(foto);
+                row.appendChild(nome);
+                row.appendChild(cidade);
+                row.appendChild(email);
+                row.appendChild(celular);
+                tableBodyElement.appendChild(row);
+            });
+        })
+        .catch(error => {
+            console.log('Ocorreu um erro:', error);
         });
-    })
-    .catch(error => {
-        console.log('Ocorreu um erro:', error);
-    });
 
 });
 
@@ -163,39 +183,39 @@ botao4.addEventListener('click', function(){
 
 let botao5 = document.getElementById('botao5');
 
-botao5.addEventListener('click', function(){
+botao5.addEventListener('click', function () {
 
     fetch('https://randomuser.me/api/?nat=BR&results=15&seed=SL&page=5')
 
-    .then(response => response.json())
+        .then(response => response.json())
 
-    .then(data => {
-        const users = data.results;
-        users.forEach(user => {
-            const row = document.createElement('tr');
-            const nome = document.createElement('td');
-            const cidade = document.createElement('td');
-            const email = document.createElement('td');
-            const celular = document.createElement('td');
-            const foto = document.createElement('img');
+        .then(data => {
+            const users = data.results;
+            users.forEach(user => {
+                const row = document.createElement('tr');
+                const nome = document.createElement('td');
+                const cidade = document.createElement('td');
+                const email = document.createElement('td');
+                const celular = document.createElement('td');
+                const foto = document.createElement('img');
 
-            nome.textContent = user.name.first;
-            cidade.textContent = user.location.city;
-            email.textContent = user.email;
-            celular.textContent = user.phone;
-            foto.src = user.picture.large;
+                nome.textContent = user.name.first;
+                cidade.textContent = user.location.city;
+                email.textContent = user.email;
+                celular.textContent = user.phone;
+                foto.src = user.picture.large;
 
-            row.appendChild(foto);
-            row.appendChild(nome);
-            row.appendChild(cidade);
-            row.appendChild(email);
-            row.appendChild(celular);
-            tableBodyElement.appendChild(row);
+                row.appendChild(foto);
+                row.appendChild(nome);
+                row.appendChild(cidade);
+                row.appendChild(email);
+                row.appendChild(celular);
+                tableBodyElement.appendChild(row);
+            });
+        })
+        .catch(error => {
+            console.log('Ocorreu um erro:', error);
         });
-    })
-    .catch(error => {
-        console.log('Ocorreu um erro:', error);
-    });
 
 });
 
@@ -203,40 +223,40 @@ botao5.addEventListener('click', function(){
 
 let botao6 = document.getElementById('botao6');
 
-botao6.addEventListener('click', function(){
+botao6.addEventListener('click', function () {
 
     fetch('https://randomuser.me/api/?nat=BR&results=15&seed=SL&page=7')
 
-    .then(response => response.json())
+        .then(response => response.json())
 
-    .then(data => {
-        const users = data.results;
-        users.forEach(user => {
-            const row = document.createElement('tr');
-            const nome = document.createElement('td');
-            const cidade = document.createElement('td');
-            const email = document.createElement('td');
-            const celular = document.createElement('td');
-            const foto = document.createElement('img');
+        .then(data => {
+            const users = data.results;
+            users.forEach(user => {
+                const row = document.createElement('tr');
+                const nome = document.createElement('td');
+                const cidade = document.createElement('td');
+                const email = document.createElement('td');
+                const celular = document.createElement('td');
+                const foto = document.createElement('img');
 
-            nome.textContent = user.name.first;
-            cidade.textContent = user.location.city;
-            email.textContent = user.email;
-            celular.textContent = user.phone;
-            foto.src = user.picture.large;
+                nome.textContent = user.name.first;
+                cidade.textContent = user.location.city;
+                email.textContent = user.email;
+                celular.textContent = user.phone;
+                foto.src = user.picture.large;
 
-            row.appendChild(foto);
-            row.appendChild(nome);
-            row.appendChild(cidade);
-            row.appendChild(email);
-            row.appendChild(celular);
-            tableBodyElement.appendChild(row);
-            
+                row.appendChild(foto);
+                row.appendChild(nome);
+                row.appendChild(cidade);
+                row.appendChild(email);
+                row.appendChild(celular);
+                tableBodyElement.appendChild(row);
+
+            });
+        })
+        .catch(error => {
+            console.log('Ocorreu um erro:', error);
         });
-    })
-    .catch(error => {
-        console.log('Ocorreu um erro:', error);
-    });
 
 });
 
